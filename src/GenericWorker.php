ce7a3727629a84c4f37c2570ae3865704716e19f
kirillreutski <k.reutsky@possibilit.nl>
2023-04-01 22:50:10 +0200

    fixed issues

--- src/GenericWorker.php
+++ src/GenericWorker.php
@@ -5,35 +5,51 @@ namespace kirillreutski\PhpWorker;
 use Step;
  
 class GenericWorker {
-    private int $id;
+    public static string $STATUS_DONE = 'done';
+    public static string $STATUS_AWAITING_NEXT_RUN = 'awaitingNextRun';
+    public static string $STATUS_IN_PROGRESS = 'inProgress';
     public array $data = [];
     public static array $steps = [];
-    public ?Step $currentStep = null;
+    public ?\kirillreutski\PhpWorker\Step $currentStep = null;
     public int $currentStepNumber = 0;
-    private array $workerRecord; 
+    private array $workerRecord = []; 
     public function __construct(array $wr){
         $this->workerRecord = $wr;
 
-        if ($this->workerRecord['current_step'] == 'done') throw new \Exception('Worker already done');
-        foreach (static::$steps as $k => $step) {
-            if ($step->name == $this->workerRecord['current_step']) {
-                $this->currentStep = $step;
-                $this->currentStepNumber = $k;
+        if (isset($this->workerRecord['current_step']) ) {
+            if ($this->workerRecord['current_step'] == 'done') throw new \Exception('Worker already done');
+            foreach (static::$steps as $k => $step) {
+                if ($step->name == $this->workerRecord['current_step']) {
+                    $this->currentStep = $step;
+                    $this->currentStepNumber = $k;
+                }
             }
         }
+        
 
         if ($this->currentStep === null) {
-            $this->currentStep = static::$steps[0];
+            if (isset(static::$steps[0])) {
+                $this->currentStep = static::$steps[0];
+            } else {
+                throw new \Exception('No steps defined');
+            }
+            
         }
-        if ($this->workerRecord['data'] === null || $this->workerRecord['data'] === '') $this->data = [];
-        else {
-            $this->data = json_decode($this->workerRecord['data'], true);
+        if (isset($this->workerRecord['data'])) {
+            if ($this->workerRecord['data'] === null || $this->workerRecord['data'] === '') $this->data = [];
+            else {
+                $this->data = json_decode($this->workerRecord['data'], true);
+            }
+        } else {
+            $this->data = []; 
         }
+        
         $this->updateStatusToInProgress();
     }
     public static function init(array $data, string $handlerName = null ) {
-        if ($handlerName === null) $handlerName = get_class(self);
-        return new ($workerRecord->handler)($data);
+        
+        if ($handlerName === null) $handlerName = static::class;
+        return new ($handlerName)($data);
     }
 
     public function goToNextStep(){
@@ -76,28 +92,32 @@ class GenericWorker {
 
     public function suspend(){
         if ($this->currentStep !== null){
-            $this->workerRecord->current_step = $this->currentStep->name;
+            $this->workerRecord['current_step'] = $this->currentStep->name;
         } else {
-            $this->workerRecord->current_step = 'done';
+            $this->workerRecord['current_step'] = 'done';
         }
-        $this->workerRecord->data = json_encode($this->data);
-        $this->workerRecord->handling_status = Statuses::$waitingNextRun;
-        $this->workerRecord->save();
+        $this->workerRecord['data'] = json_encode($this->data);
+        $this->workerRecord['handling_status'] = static::$STATUS_AWAITING_NEXT_RUN;
+        $this->saveState(); 
+    }
+
+    public function saveState(){
+
     }
 
     public function updateStatusToInProgress(): void
     {
-        $this->updateStatus('inProgress');
+        $this->updateStatus(static::$STATUS_IN_PROGRESS);
     }
 
     public function updateStatusToAwaitingNextRun(): void
     {
-        $this->updateStatus('awaitingNextRun');
+        $this->updateStatus(static::$STATUS_AWAITING_NEXT_RUN);
     }
 
     public function updateStatusToDone(): void
     {
-        $this->updateStatus('done');
+        $this->updateStatus(static::$STATUS_DONE);
     }
 
     public function updateStatus(string $newStatus): void{
